ea20fcf947a2256f1c4e92a62e44315de9449e5f
Thiago Leite <thiago.0686@gmail.com>
2025-12-13 19:10:43 -0300

    feat: First structure created

---
+++ src/main/java/com/thiagoleite06/smart_agenda/SmartAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoleite06.smart_agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagoleite06/smart_agenda/SmartAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoleite06.smart_agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
